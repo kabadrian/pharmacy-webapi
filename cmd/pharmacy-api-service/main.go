8c24d529279509fa1767c9674f26d7344ea0fa52
Adrian Kabac <kabadrian2@gmail.com>
2024-04-27 09:23:54 +0200

    Add db service for mongodb

--- cmd/pharmacy-api-service/main.go
+++ cmd/pharmacy-api-service/main.go
@@ -8,6 +8,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kabadrian/pharmacy-webapi/api"
 	"github.com/kabadrian/pharmacy-webapi/internal/pharmacy_pl"
+
+	"context"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/kabadrian/pharmacy-webapi/internal/db_service"
 )
 
 func main() {
@@ -22,6 +28,24 @@ func main() {
 	}
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+
+	engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
 	// request routings
 	pharmacy_pl.AddRoutes(engine)
 	engine.GET("/openapi", api.HandleOpenApi)
